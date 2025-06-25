919a572859ce2936401fff3386a30eb32d3784c4
charmingruby <mrrubycanal3105@gmail.com>
2025-06-25 15:03:15 -0300

    refactor: simplifies context

--- internal/device/delivery/queue/queue.go
+++ internal/device/delivery/queue/queue.go
@@ -5,7 +5,3 @@ import "github/charmingruby/habits/internal/device/delivery/queue/message"
 type DeviceQueue interface {
 	OnDeviceConnected(msg message.OnDeviceConnectedMessage) error
 }
-
-type ApplicationQueue interface {
-	DispatchDeviceCreated(msg message.DispatchDeviceCreatedMessage) error
-}
