ade917f914bcf4a54d7fe5252f384de5543dfca7
charmingruby <mrrubycanal3105@gmail.com>
2025-06-28 11:18:35 -0300

    chore: moves device mqtt logic into mqtt

--- internal/device/delivery/broker/mqtt/mqtt.go
+++ internal/device/delivery/broker/mqtt/mqtt.go
@@ -1,5 +1,16 @@
 package mqtt
 
-const DEFAULT_QOS_LEVEL = 1
+import "fmt"
+
+const (
+	DEFAULT_QOS_LEVEL = 1
+
+	SERVER_ORIGIN   = "api"
+	FIRMWARE_ORIGIN = "firmware"
+)
 
 type HandlerFunc func(msg []byte) error
+
+func buildTopic(deviceID, event, origin string) string {
+	return fmt.Sprintf("devices/%s/event/%s/from/%s", deviceID, event, origin)
+}
