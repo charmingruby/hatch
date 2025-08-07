4efb41cc34220b435e15def6b7312031605a2da4
charmingruby <mrrubycanal3105@gmail.com>
2025-08-07 19:54:54 -0300

    feat: adds communication layer doc

--- internal/platform/platform.go
+++ internal/platform/platform.go
@@ -1,15 +1,12 @@
 package platform
 
 import (
-	"github.com/charmingruby/pack/internal/platform/http/rest"
+	"github.com/charmingruby/pack/internal/platform/http/endpoint"
 	"github.com/charmingruby/pack/pkg/database/postgres"
 
 	"github.com/gin-gonic/gin"
 )
 
-func New(
-	r *gin.Engine,
-	db *postgres.Client,
-) {
-	rest.New(r, db)
+func New(r *gin.Engine, db *postgres.Client) {
+	endpoint.New(r, db)
 }
